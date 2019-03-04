4e040027846216269864153b73956b17d9749a5b
houssagi <gwen22h@gmail.com>
2019-03-04 14:29:46 +0100

    Liens bouton connexion et about

--- app/src/main/java/com/desbois/mathis/bizzbee/AboutActivity.java
+++ app/src/main/java/com/desbois/mathis/bizzbee/AboutActivity.java
@@ -3,8 +3,9 @@ package com.desbois.mathis.bizzbee;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends AppCompatActivity {
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
